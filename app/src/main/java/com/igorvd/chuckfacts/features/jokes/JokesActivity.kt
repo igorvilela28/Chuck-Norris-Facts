32e28db4943335e71a5001993f8d6cbff5100ef9
Igor Vilela <igorvilela28@hotmail.com>
2019-04-02 16:26:17 -0300

    Improve readability

--- app/src/main/java/com/igorvd/chuckfacts/features/jokes/JokesActivity.kt
+++ app/src/main/java/com/igorvd/chuckfacts/features/jokes/JokesActivity.kt
@@ -7,52 +7,63 @@ import androidx.appcompat.app.AppCompatActivity
 import com.igorvd.chuckfacts.R
 import com.igorvd.chuckfacts.features.search.SearchJokeActivity
 import kotlinx.android.synthetic.main.activity_jokes.*
-import android.view.animation.BounceInterpolator
-import android.view.animation.OvershootInterpolator
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.transition.ChangeBounds
-import androidx.transition.Transition
 import androidx.transition.TransitionManager
 import com.igorvd.chuckfacts.utils.extensions.hideContent
 import com.igorvd.chuckfacts.utils.extensions.showContent
 import com.igorvd.chuckfacts.utils.transition.TransitionsFactory
 
+typealias LayoutParams = ConstraintLayout.LayoutParams
+
 class JokesActivity : AppCompatActivity() {
 
-    private val paramsHolder: Pair<ConstraintLayout.LayoutParams, ConstraintLayout.LayoutParams> by lazy {
-        val originalParams = cvSearchBar.layoutParams as ConstraintLayout.LayoutParams
-        val newParams = ConstraintLayout.LayoutParams(originalParams)
+    private val paramsHolder: Pair<LayoutParams, LayoutParams> by lazy {
+        val originalParams = cvSearchBar.layoutParams as LayoutParams
+        val newParams = LayoutParams(originalParams)
         newParams.setMargins(0, 0, 0, 0)
         originalParams to newParams
     }
 
+    //**************************************************************************
+    // region: LIFE CYCLE
+    //**************************************************************************
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_jokes)
-
-
-        cvSearchBar.setOnClickListener {
-
-            val transition = TransitionsFactory.fadeOutwithActionOnEnd {
-                val intent = Intent(this@JokesActivity, SearchJokeActivity::class.java).apply {
-                    addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-                }
-                startActivity(intent)
-            }
-
-            TransitionManager.beginDelayedTransition(cvSearchBar as CardView,
-                    transition)
-
-            cvSearchBar.layoutParams = paramsHolder.second
-            ((cvSearchBar as CardView).getChildAt(0) as ViewGroup).hideContent()
-
-        }
+        setupViews()
     }
 
     override fun onResume() {
         super.onResume()
+        setOriginalBar()
+    }
+
+    //endregion
+
+    //**************************************************************************
+    // region: INNER METHODS
+    //**************************************************************************
+
+    private fun setupViews() {
+        cvSearchBar.setOnClickListener {
+            val transition = TransitionsFactory.changeBoundsWithActionOnEnd(action = ::startSearchActivity)
+            TransitionManager.beginDelayedTransition(cvSearchBar as CardView, transition)
+            cvSearchBar.layoutParams = paramsHolder.second
+            ((cvSearchBar as CardView).getChildAt(0) as ViewGroup).hideContent()
+        }
+    }
+
+    private fun setOriginalBar() {
         cvSearchBar.layoutParams = paramsHolder.first
         ((cvSearchBar as CardView).getChildAt(0) as ViewGroup).showContent()
     }
+
+    private fun startSearchActivity() {
+        val intent = SearchJokeActivity.newIntent(this)
+        startActivity(intent)
+    }
+
+    //endregion
 }
--- app/src/main/java/com/igorvd/chuckfacts/features/search/SearchJokeActivity.kt
+++ app/src/main/java/com/igorvd/chuckfacts/features/search/SearchJokeActivity.kt
@@ -1,44 +1,51 @@
 package com.igorvd.chuckfacts.features.search
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ViewTreeObserver
 import com.igorvd.chuckfacts.R
-import com.igorvd.chuckfacts.utils.extensions.hideContent
 import kotlinx.android.synthetic.main.search_toolbar.*
-import androidx.transition.TransitionManager
-import com.igorvd.chuckfacts.utils.extensions.showContent
-import com.igorvd.chuckfacts.utils.transition.TransitionsFactory
 
 
 class SearchJokeActivity : AppCompatActivity() {
+
+    companion object {
+
+        fun newIntent(context: Context): Intent {
+            val intent = Intent(context, SearchJokeActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            }
+            return intent
+        }
+
+    }
+
+    //**************************************************************************
+    // region: LIFE CYCLE
+    //**************************************************************************
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_joke)
 
+        setupViews()
+
+    }
+
+    //endregion
+
+    //**************************************************************************
+    // region: INNER METHODS
+    //**************************************************************************
+
+    private fun setupViews() {
         setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
-
-        if (savedInstanceState == null) {
-
-            toolbar.hideContent()
-
-            toolbar.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
-                override fun onGlobalLayout() {
-                    toolbar.viewTreeObserver.removeOnGlobalLayoutListener(this)
-                    showSearch()
-                }
-
-            })
-
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
         }
     }
 
-    private fun showSearch() {
-        // use the TransitionManager to animate the changes of the Toolbar
-        TransitionManager.beginDelayedTransition(toolbar, TransitionsFactory.fadeInTransition())
-        toolbar.showContent()
-    }
+    //endregion
 }
