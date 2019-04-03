ded8d6cf0836e2b77199aef2a2d476db73b3db2b
Igor Vilela <igorvilela28@hotmail.com>
2019-04-03 16:41:56 -0300

    Add implementation for joke category repository

---
+++ data/src/main/java/com/igorvd/chuckfacts/data/jokes/repository/JokeCategoryRepositoryImpl.kt
@@ -0,0 +1,28 @@
+package com.igorvd.chuckfacts.data.jokes.repository
+
+import com.igorvd.chuckfacts.data.jokes.local.JokeCategoryLocalDataSource
+import com.igorvd.chuckfacts.data.jokes.remote.JokeCategoryRemoteDataSource
+import com.igorvd.chuckfacts.domain.jokes.repository.JokeCategoryRepository
+import javax.inject.Inject
+
+/**
+ * Repository that fetch categories. It has a local first policy.
+ */
+class JokeCategoryRepositoryImpl @Inject constructor(
+    private val remoteDataSource: JokeCategoryRemoteDataSource,
+    private val localDataDataSource: JokeCategoryLocalDataSource
+) : JokeCategoryRepository {
+
+    override suspend fun retrieveAll(): List<String> {
+
+        val localCategories = localDataDataSource.retrieveAll()
+
+        if (localCategories.isNotEmpty()) {
+            return localCategories
+        }
+
+        val remoteCategories = remoteDataSource.retrieveAll()
+        localDataDataSource.save(remoteCategories)
+        return remoteCategories
+    }
+}
---
+++ data/src/test/java/com/igorvd/chuckfacts/data/jokes/repository/JokeCategoryRepositoryImplTest.kt
@@ -0,0 +1,56 @@
+package com.igorvd.chuckfacts.data.jokes.repository
+
+import com.igorvd.chuckfacts.data.jokes.local.JokeCategoryLocalDataSource
+import com.igorvd.chuckfacts.data.jokes.remote.JokeCategoryRemoteDataSource
+import com.igorvd.chuckfacts.data.testutils.DUMMY_CATEGORIES
+import io.mockk.*
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class JokeCategoryRepositoryImplTest {
+
+    @MockK
+    private lateinit var remoteDataSource: JokeCategoryRemoteDataSource
+    @MockK
+    private lateinit var localDataSource: JokeCategoryLocalDataSource
+    private lateinit var jokeCategoryRepositoryImpl: JokeCategoryRepositoryImpl
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        jokeCategoryRepositoryImpl = JokeCategoryRepositoryImpl(remoteDataSource, localDataSource)
+    }
+
+    @Test
+    fun `should load categories from local data source first`() = runBlocking {
+
+        every { localDataSource.retrieveAll() } returns DUMMY_CATEGORIES
+
+        val categories = jokeCategoryRepositoryImpl.retrieveAll()
+
+        assertEquals(DUMMY_CATEGORIES, categories)
+        verify (exactly = 1) { localDataSource.retrieveAll() }
+        verify { remoteDataSource wasNot Called }
+    }
+
+    @Test
+    fun `should load categories from remote when data source is empty`() = runBlocking {
+
+        every { localDataSource.retrieveAll() } returns emptyList()
+        coEvery { remoteDataSource.retrieveAll() } returns DUMMY_CATEGORIES
+        every { localDataSource.save(DUMMY_CATEGORIES) } just Runs
+
+        val categories = jokeCategoryRepositoryImpl.retrieveAll()
+
+        assertEquals(DUMMY_CATEGORIES, categories)
+        coVerifyOrder {
+            localDataSource.retrieveAll()
+            remoteDataSource.retrieveAll()
+            localDataSource.save(categories)
+        }
+    }
+}
