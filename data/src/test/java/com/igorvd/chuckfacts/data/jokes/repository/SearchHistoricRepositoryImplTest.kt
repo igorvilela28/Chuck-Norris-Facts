ce81aa721184e6c4187b5b40c297b14438a1d21e
Igor Vilela <igorvilela28@hotmail.com>
2019-04-08 17:14:48 -0300

    Improve unit tests

--- data/src/test/java/com/igorvd/chuckfacts/data/jokes/repository/SearchHistoricRepositoryImplTest.kt
+++ data/src/test/java/com/igorvd/chuckfacts/data/jokes/repository/SearchHistoricRepositoryImplTest.kt
@@ -2,7 +2,6 @@ package com.igorvd.chuckfacts.data.jokes.repository
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
-import com.igorvd.chuckfacts.data.testutils.DUMMY_CATEGORIES
 import com.igorvd.chuckfacts.data.testutils.DUMMY_SEARCH_HISTORIC
 import org.junit.Before
 
@@ -14,20 +13,26 @@ import org.robolectric.RobolectricTestRunner
 @RunWith(RobolectricTestRunner::class)
 class SearchHistoricRepositoryImplTest {
 
-    private lateinit var jokeCategoryLocalDataSourceImpl: SearchHistoricRepositoryImpl
+    private lateinit var searchHistoricRepositoryImpl: SearchHistoricRepositoryImpl
     private lateinit var context: Context
 
     @Before
     fun setUp() {
         context = ApplicationProvider.getApplicationContext()
-        jokeCategoryLocalDataSourceImpl = SearchHistoricRepositoryImpl(context)
+        searchHistoricRepositoryImpl = SearchHistoricRepositoryImpl(context)
     }
 
     @Test
     fun `should save search historic on local data source`() {
-        jokeCategoryLocalDataSourceImpl.save(DUMMY_SEARCH_HISTORIC)
-        val historic = jokeCategoryLocalDataSourceImpl.retrieveAll()
+        searchHistoricRepositoryImpl.save(DUMMY_SEARCH_HISTORIC)
+        val historic = searchHistoricRepositoryImpl.retrieveAll()
         assertEquals(DUMMY_SEARCH_HISTORIC, historic)
+    }
+
+    @Test
+    fun `should retrieve empty search historic`() {
+        val historic = searchHistoricRepositoryImpl.retrieveAll()
+        assertEquals(emptyList<String>(), historic)
     }
 
 }
