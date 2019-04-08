ab1da842f8f9ebb4b83395314d8ea9db5f00e125
Igor Vilela <igorvilela28@hotmail.com>
2019-04-08 16:54:24 -0300

    Add unit test for interactors

---
+++ domain/src/test/java/com/igorvd/chuckfacts/domain/jokes/interactor/AddQueryToSearchHistoricInteractorTest.kt
@@ -0,0 +1,103 @@
+package com.igorvd.chuckfacts.domain.jokes.interactor
+
+import com.igorvd.chuckfacts.domain.jokes.repository.SearchHistoricRepository
+import com.igorvd.chuckfacts.domain.utils.DUMMY_SEARCH_HISTORIC
+import io.mockk.*
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class AddQueryToSearchHistoricInteractorTest {
+
+    @MockK
+    private lateinit var searchHistoricRepository: SearchHistoricRepository
+    private lateinit var interactor: AddQueryToSearchHistoricInteractor
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        interactor = AddQueryToSearchHistoricInteractor(searchHistoricRepository)
+    }
+
+    @Test
+    fun `should save query when current historic is empty`() = runBlocking {
+
+        every { searchHistoricRepository.retrieveAll() } returns emptyList()
+        every { searchHistoricRepository.save(listOf("dev")) } just Runs
+
+
+        val params = AddQueryToSearchHistoricInteractor.Params("dev")
+        val created = interactor.execute(params)
+
+        verifySequence {
+            searchHistoricRepository.retrieveAll()
+            searchHistoricRepository.save(listOf("dev"))
+        }
+
+    }
+
+    @Test
+    fun `should save new queries on first list position`() = runBlocking {
+
+        every { searchHistoricRepository.retrieveAll() } returns DUMMY_SEARCH_HISTORIC.take(3)
+        val expectedHistoric = DUMMY_SEARCH_HISTORIC.take(3).toMutableList().apply {
+            add(0, "infinite")
+        }
+        every { searchHistoricRepository.save(expectedHistoric) } just Runs
+
+
+        val params = AddQueryToSearchHistoricInteractor.Params("infinite")
+        val created = interactor.execute(params)
+
+        verifySequence {
+            searchHistoricRepository.retrieveAll()
+            searchHistoricRepository.save(expectedHistoric)
+        }
+
+    }
+
+    @Test
+    fun `should save and shift query to first position when current historic already has the query`() = runBlocking {
+
+        every { searchHistoricRepository.retrieveAll() } returns DUMMY_SEARCH_HISTORIC.take(3)
+        val expectedHistoric = DUMMY_SEARCH_HISTORIC.take(3).toMutableList().apply {
+            remove("dev")
+            add(0, "Dev")
+        }
+        every { searchHistoricRepository.save(expectedHistoric) } just Runs
+
+
+        val params = AddQueryToSearchHistoricInteractor.Params("Dev")
+        val created = interactor.execute(params)
+
+        verifySequence {
+            searchHistoricRepository.retrieveAll()
+            searchHistoricRepository.save(expectedHistoric)
+        }
+
+    }
+
+    @Test
+    fun `should drop outdated query when max query historic limit is reached`() = runBlocking {
+
+        every { searchHistoricRepository.retrieveAll() } returns DUMMY_SEARCH_HISTORIC.take(9)
+        val expectedHistoric = DUMMY_SEARCH_HISTORIC.take(9).toMutableList().apply {
+            add(0, "infinite")
+        }
+        every { searchHistoricRepository.save(expectedHistoric) } just Runs
+
+
+        val params = AddQueryToSearchHistoricInteractor.Params("infinite")
+        val created = interactor.execute(params)
+
+        verifySequence {
+            searchHistoricRepository.retrieveAll()
+            searchHistoricRepository.save(expectedHistoric)
+        }
+
+    }
+
+
+}
---
+++ domain/src/test/java/com/igorvd/chuckfacts/domain/utils/DummySearchHistoric.kt
@@ -0,0 +1,4 @@
+package com.igorvd.chuckfacts.domain.utils
+
+val DUMMY_SEARCH_HISTORIC = listOf("explicit", "dev", "movie", "food", "celebrity", "science", "sport",
+        "political", "religion", "animal")
