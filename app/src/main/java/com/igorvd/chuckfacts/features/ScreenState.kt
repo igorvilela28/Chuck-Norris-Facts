9893ff376929069e282dae84d091681d142cd692
Igor Vilela <igorvilela28@hotmail.com>
2019-04-10 11:31:26 -0300

    Improve readability

--- app/src/main/java/com/igorvd/chuckfacts/features/ScreenState.kt
+++ app/src/main/java/com/igorvd/chuckfacts/features/ScreenState.kt
@@ -7,7 +7,7 @@ object NetworkError : ScreenState()
 object HttpError : ScreenState()
 object EmptyResult : ScreenState()
 
-//FIXME: move this when it becomes possible to inherit from sealed class from another file
+//FIXME: move this when it becomes possible to inherit from sealed class from another file or maybe use generics
 sealed class JokeScreenState : ScreenState() {
     data class Result(val result: List<JokeView>) : JokeScreenState()
 }
