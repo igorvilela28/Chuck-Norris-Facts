6e9e665bf63f5a406f19c2150b48066e64e8189b
Igor Vilela <igorvilela28@hotmail.com>
2019-04-11 11:29:17 -0300

    Improve imports

--- data/src/main/java/com/igorvd/chuckfacts/data/jokes/repository/JokeRepositoryImpl.kt
+++ data/src/main/java/com/igorvd/chuckfacts/data/jokes/repository/JokeRepositoryImpl.kt
@@ -1,11 +1,8 @@
 package com.igorvd.chuckfacts.data.jokes.repository
 
-import com.igorvd.chuckfacts.data.jokes.local.JokeCategoryLocalDataSource
 import com.igorvd.chuckfacts.data.jokes.local.JokeLocalDataSource
-import com.igorvd.chuckfacts.data.jokes.remote.JokeCategoryRemoteDataSource
 import com.igorvd.chuckfacts.data.jokes.remote.JokeRemoteDataSource
 import com.igorvd.chuckfacts.domain.jokes.entity.Joke
-import com.igorvd.chuckfacts.domain.jokes.repository.JokeCategoryRepository
 import com.igorvd.chuckfacts.domain.jokes.repository.JokeRepository
 import javax.inject.Inject
 
